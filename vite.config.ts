698e2ef33eb2caedee5431b99dca5ce5e0a5a1af
jardelMessias39 <jardelmessias28@gmail.com>
2025-09-26 00:49:28 -0300

    recente

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,14 +11,5 @@ export default defineConfig(({ mode }) => ({
     base: '/brasil-mais-belo/',
     open: true,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+ 
 }));
